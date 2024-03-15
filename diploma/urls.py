d1d6c5ceae4ce7916f307161d604e394dc82a46a
NurSoult <200103098@stu.sdu.edu.kz>
2024-03-15 14:44:22 +0500

    added: requirements.txt

--- diploma/urls.py
+++ diploma/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from ..auth.views import CustomTokenObtainPairView
+from authenticate.views import CustomTokenObtainPairView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('auth/', include('auth.urls')),
+    path('auth/', include('authenticate.urls')),
 
     path('auth/login', CustomTokenObtainPairView.as_view(), name='login'),
 ]
